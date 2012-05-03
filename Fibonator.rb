a7fddce130b8cf078b89a28a53a311f3c2594af1
Joshua Wilson <jbw04@live.com>
2012-05-02 22:42:07 -0400

    this is a comment that conforms to regs

---
+++ Fibonator.rb
@@ -0,0 +1,24 @@
+puts "The Ultimate Fibonator"
+
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+# find the sum of the even-valued terms.
+
+def GetMeMyNumber(first, second)
+  first + second
+end
+
+sum = 0
+first = 0
+second = 1
+i = GetMeMyNumber(first, second)
+while  i <= 4000000 do
+  puts i
+  if  i % 2 == 0
+    sum += i
+  end
+  first = second
+  second = i
+  i = GetMeMyNumber(first, second)
+end
+
+puts "TOTAL " + sum.to_s
